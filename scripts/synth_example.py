5118ea344b2255051ec164faa6d25033252e76a3
Adrian Benson <a.benson@gns.cri.nz>
2020-11-16 15:39:39 +1300

    Python 3'ise scripts

--- scripts/synth_example.py
+++ scripts/synth_example.py
@@ -1,10 +1,12 @@
 from synthpop.recipes.starter2 import Starter
-from synthpop.synthesizer import synthesize_all, enable_logging 
+from synthpop.synthesizer import synthesize_all
 import os
+
 
 def synthesize_county(county):
     starter = Starter(os.environ["CENSUS"], "CO", county)
     synthetic_population = synthesize_all(starter)
     return synthetic_population
-    
+
+
 synthesize_county('Gilpin County')
